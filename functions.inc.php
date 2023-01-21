bf106949ca479f8b31251518399766b55883b4c6
grumich <gartlain@mail.ru>
2023-01-21 21:46:45 +0300

    Second commit

--- functions.inc.php
+++ functions.inc.php
@@ -129,47 +129,4 @@ function getPerfectPartner($surname, $name, $patronomyc, $array){
     ♡ Идеально на $percent% ♡
 HEREDOC;
 }
-
-//Разбиение 
-foreach ($persons_array  as $key=>$val) {
-	
-                $Fio = $persons_array[$key]['fullname'] . '<br>';
-                $seporated = getPartsFromFullname($Fio);
-
-                echo $seporated['surname']." ";
-                echo "<br/>";
-                echo $seporated['name']." ";
-                echo "<br/>";
-                echo $seporated['patronomyc'];
-                echo "<br/>";
-}
-
-//объединение 
-foreach ($persons_array  as $key=>$val) {
-	
-    $Fio = $persons_array[$key]['fullname'] . '<br>';
-    $seporated = getPartsFromFullname($Fio);
-    $tr = getFullnameFromParts($seporated['surname'],$seporated['name'],$seporated['patronomyc']);
-    echo $tr;
-
-}
-//Сокращение ФИО
-  echo '<br>';
-    foreach ($persons_array  as $key=>$val) {
-	    $Fio = $persons_array[$key]['fullname'] . '<br>';
-        $shortName=getShortName($Fio);
-        echo $shortName. '<br>';
-}
-//Функция определения пола по ФИО
-echo '<br>';
-foreach ($persons_array  as $key=>$val) {
-    $Fio = $persons_array[$key]['fullname'];
-    $Gender=getGenderFromName($Fio);
-    echo $Gender. '<br>';
-}
-//Определение возрастно-полового состава
-getGenderDescription($persons_array);
-//Идеальный подбор пары
-//getPerfectPartner("АлександРоВ","ИваН","ГриГоРьевиЧ",$persons_array);
-getPerfectPartner("АлександРоВа","Инна","ГриГоРьевнА",$persons_array);
 ?>
--- index.php
+++ index.php
@@ -3,8 +3,48 @@
 include "main.php";
 
 
-// getPerfectPartner("Бардо", "Жаклин", "Фёдоровна", $persons_array);
-//getGenderDescription($persons_array);
+//Разбиение 
+foreach ($persons_array  as $key=>$val) {
+	
+    $Fio = $persons_array[$key]['fullname'] . '<br>';
+    $seporated = getPartsFromFullname($Fio);
 
+    echo $seporated['surname']." ";
+    echo "<br/>";
+    echo $seporated['name']." ";
+    echo "<br/>";
+    echo $seporated['patronomyc'];
+    echo "<br/>";
+}
+
+//объединение 
+foreach ($persons_array  as $key=>$val) {
+
+$Fio = $persons_array[$key]['fullname'] . '<br>';
+$seporated = getPartsFromFullname($Fio);
+$tr = getFullnameFromParts($seporated['surname'],$seporated['name'],$seporated['patronomyc']);
+echo $tr;
+
+}
+//Сокращение ФИО
+echo '<br>';
+foreach ($persons_array  as $key=>$val) {
+$Fio = $persons_array[$key]['fullname'] . '<br>';
+$shortName=getShortName($Fio);
+echo $shortName. '<br>';
+}
+//Функция определения пола по ФИО
+echo '<br>';
+foreach ($persons_array  as $key=>$val) {
+$Fio = $persons_array[$key]['fullname'];
+$Gender=getGenderFromName($Fio);
+echo $Gender. '<br>';
+}
+//Определение возрастно-полового состава
+getGenderDescription($persons_array);
+
+//Идеальный подбор пары
+getPerfectPartner("АлександРоВ","ГенаДий","ГриГоРьевиЧ",$persons_array);
+getPerfectPartner("АлександРоВа","Инна","ГриГоРьевнА",$persons_array);
 
 ?>
